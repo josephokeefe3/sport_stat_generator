797da3c52f0d7900ebbeb485b66c54fb05325ba7
rbertagna <rbertagna@riverdale.edu>
2015-07-16 12:29:05 -0400

    Fix plus button, add minus button

--- js/stat-enter-soccer.js
+++ js/stat-enter-soccer.js
@@ -1,8 +1,34 @@
 $(document).ready(function(){
+
+  $('body > div.wrapper > ul > li:nth-child(1)').click(function(){
+    $('#field-players').addClass('open');
+    $('#goalies').removeClass('open');
+    $('body > div.wrapper > ul > li:nth-child(1)').addClass('active');
+    $('body > div.wrapper > ul > li:nth-child(2)').removeClass('active');
+  });
+  $('body > div.wrapper > ul > li:nth-child(2)').click(function(){
+    $('#goalies').addClass('open');
+    $('#field-players').removeClass('open');
+    $('body > div.wrapper > ul > li:nth-child(2)').addClass('active');
+    $('body > div.wrapper > ul > li:nth-child(1)').removeClass('active');
+  });
+
   var stat_row = '<tr class="stat_row"><td> <input type="text" class="stat"> </td><td> <input type="text" class="stat"> </td> <td> <input type="text" class="stat">  </td><td> <input type="text" class="stat">  </td><td> <input type="text" class="stat">  </td><td> <input type="text" class="stat">  </td><td> <input type="text" class="stat">  </td></tr>';
 
-  $('.add_player').click(function(){
-    $('.stat_table').append(stat_row);
-
+  $('#field-players-add').click(function(){
+    $('#field-players-stat').append(stat_row);
   });
+
+  $('#goalies-add').click(function(){
+    $('#goalies-stat').append(stat_row);
+  });
+
+  $('#field-players-sub').click(function(){
+    $('.stat_row:last-child' ,'#field-players-stat').remove();
+  });
+
+  $('#goalies-sub').click(function(){
+    $('.stat_row:last-child', '#goalies-stat').remove();
+  });
+
 });
