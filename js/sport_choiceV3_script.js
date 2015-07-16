fa7d6d30a36d3f26c8a47c57ab1032d6e66980b5
Joseph O'Keefe <josephokeefe@Josephs-Air.home>
2015-07-16 19:29:27 -0400

    finished soccer stat choice global variable assignment

--- js/sport_choiceV3_script.js
+++ js/sport_choiceV3_script.js
@@ -1,9 +1,13 @@
 $(function(){
 
+	sessionStorage.num_general_stats = 0;
+	sessionStorage.num_goalie_stats = 0;
+
 	$('.choice_button #football_choice').click(function(){
 		$('.choice_button div').slideUp(400);
 		$('#football_stat_options').delay(400).fadeIn(200);
 		$('.choice_button').delay(400).slideUp(400);
+		console.log(sessionStorage.num_general_stats);
 	});
 
 	$('.choice_button #baseball_choice').click(function(){
@@ -25,13 +29,33 @@ $(function(){
 		$('.choice_button').delay(400).slideUp(400);
 	});
 
-	$('td').click(function(){
+	$('#general td').click(function(){
 		if ($(this).attr("data-clicked") == 0){
 			$(this).css({"color": "white", "background-color": "#0E76FF"});
 			$(this).attr("data-clicked", "1");
+			sessionStorage.setItem($(this).attr("id"), "true");
+			sessionStorage.num_general_stats = String(parseInt(sessionStorage.num_general_stats) + 1);
 		} else {
 			$(this).css({"color": "black", "background-color": "white"});
 			$(this).attr("data-clicked", "0");
+			sessionStorage.setItem($(this).attr("id"), "false");
+			sessionStorage.num_general_stats = String(parseInt(sessionStorage.num_general_stats) - 1);
 		}
+		console.log("Number of general stats: " + sessionStorage.num_general_stats);
+	});
+
+	$('#goalie td').click(function(){
+		if ($(this).attr("data-clicked") == 0){
+			$(this).css({"color": "white", "background-color": "#0E76FF"});
+			$(this).attr("data-clicked", "1");
+			sessionStorage.setItem($(this).attr("id"), "true");
+			sessionStorage.num_goalie_stats = String(parseInt(sessionStorage.num_goalie_stats) + 1);
+		} else {
+			$(this).css({"color": "black", "background-color": "white"});
+			$(this).attr("data-clicked", "0");
+			sessionStorage.setItem($(this).attr("id"), "false");
+			sessionStorage.num_goalie_stats = String(parseInt(sessionStorage.num_goalie_stats) - 1);
+		}
+		console.log("Number of goalie stats: " + sessionStorage.num_goalie_stats);
 	});
 });
