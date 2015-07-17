c4be640b2b22df5290ba523bdd7098a5f4b75088
Joseph O'Keefe <josephokeefe@Josephs-MacBook-Air.local>
2015-07-17 09:20:50 -0400

    finished dynamic table

--- js/stat-enter-soccerV2.js
+++ js/stat-enter-soccerV2.js
@@ -18,6 +18,40 @@ $(document).ready(function(){
     for (y = 0; y < num_general_stats; y++){
     $('body > div.wrapper > div.general_stat_table > table > tbody > tr:last-of-type').append("<td id='" + included_general_stats_array[y] + "'><input type='text' class='input_box' placeholder='" + included_general_stats_array[y] + "'</td>");
     console.log(y);
+  }
+  });
+
+  $('#field_players_sub').click(function(){
+    $('tr:last-of-type', 'body > div.wrapper > div.general_stat_table > table > tbody').remove();
+  });
+
+    for (i = 0; i < num_goalie_stats; i++){
+    $('body > div.wrapper > div.goalie_stat_table > table > thead > tr').append("<td id='" + included_goalie_stats_array[i] + "'>" + included_goalie_stats_array[i] + "</td>");
+    $('body > div.wrapper > div.goalie_stat_table > table > tbody > tr').append("<td id='" + included_goalie_stats_array[i] + "'><input type='text' class='input_box' placeholder='" + included_goalie_stats_array[i] + "'</td>");
+  }
+
+  $('#goalie_players_add').click(function(){
+    $('body > div.wrapper > div.goalie_stat_table > table > tbody').append("<tr><td><input type='text' class='input_box' placeholder='Player'></td></tr>");
+    for (y = 0; y < num_goalie_stats; y++){
+    $('body > div.wrapper > div.goalie_stat_table > table > tbody > tr:last-of-type').append("<td id='" + included_goalie_stats_array[y] + "'><input type='text' class='input_box' placeholder='" + included_goalie_stats_array[y] + "'</td>");
+    console.log(y);
+  }
+  });
+
+  $('#goalie_players_sub').click(function(){
+    $('tr:last-of-type', 'body > div.wrapper > div.goalie_stat_table > table > tbody').remove();
+  });
+
+  $('#nav_wrapper ul li:last-of-type').click(function(){
+    if ($(this).attr('class') !== "active"){
+      $(this).addClass('active');
+      $('#nav_wrapper ul li:first-of-type').removeClass('active');
+      $('.general_stat_table').fadeOut(100);
+      $('.goalie_stat_table').delay(100).fadeIn(100);
+
+    }
+    else {
+      return;
     }
   });
 
@@ -25,24 +59,20 @@ $(document).ready(function(){
     if ($(this).attr('class') !== "active"){
       $(this).addClass('active');
       $('#nav_wrapper ul li:last-of-type').removeClass('active');
-      $('#general_stat_table').delay(200).fadeIn(200);
-      $('#goalie_stat_table').fadeOut(200);
+      $('.general_stat_table').delay(100).fadeIn(100);
+      $('.goalie_stat_table').fadeOut(100);
     }
     else {
       return;
     }
   });
 
-  $('#nav_wrapper ul li:last-of-type').click(function(){
-    if ($(this).attr('class') !== "active"){
-      $(this).addClass('active');
-      $('#nav_wrapper ul li:first-of-type').removeClass('active');
-      $('#general_stat_table').fadeOut(200);
-      $('#goalie_stat_table').delay(200).fadeIn(200);
-    }
-    else {
-      return;
-    }
-  });
+
+
+
+
+
+
+  
 
 });
